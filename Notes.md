e1d880e9eb0b14a9ba7316dde474aabd8b922e41
Rajender Tella <rajender.tella@gmail.com>
2018-09-18 15:04:09 -0500

    Introducing test doubles

--- Notes.md
+++ Notes.md
@@ -23,3 +23,5 @@ Smell 9: Separation of concerns. The address related activities can be separated
 Smell 10: Reviewing the Address, it can further refactored to separate city sate lookup from address class.
 
 Smell 11: Separation of concerns. The city state look up doesn't have to be part of address or city state domain. That could be a separate utility service.
+
+Smell 12: Integration tests are bundled with unit tests. The city state look up service makes a remote call to get information. This need to be separated from unit tests. Moved the integration tests to a separate package for now. Adding a unit test using test doubles.
--- src/test/java/com/neopragma/legacy/screen/JobApplicantTest.java
+++ src/test/java/com/neopragma/legacy/screen/JobApplicantTest.java
@@ -1,34 +1,61 @@
 package com.neopragma.legacy.screen;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import com.neopragma.legacy.entity.CityState;
+import com.neopragma.legacy.utils.CityStateLookup;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.neopragma.legacy.entity.JobApplicant;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 /**
  * Automated unit checks for the base version of the JobApplicant application.
- * This version of the code contains a number of code smells that may point to
- * potential improvements in the design of the code.
+ * Contains unit tests for city state lookup service.
+ * TODO: Revisit the placement of this test
  * 
  * @author neopragma
  * @version 1.0.0
  * @since 1.7
  */
 
+@RunWith(MockitoJUnitRunner.class)
 public class JobApplicantTest {
-	
-	private JobApplicant jobApplicant;
-	
-	@Before
-	public void beforeEach() {
-		jobApplicant = new JobApplicant();
+
+	@Mock
+	private CityStateLookup cityStateLookup;
+
+	@Test
+	public void itFindsAddisonTexasBy5DigitZipCode() throws URISyntaxException, IOException {
+		when(cityStateLookup.findCityStateBasedOnZipCode("75001")).thenReturn(new CityState("Addison", "TX"));
+		assertCityState("75001", "Addison", "TX");
 	}
-	
 
+	@Test
+	public void itFindsMaranaArizonaBy9DigitZipCode() throws URISyntaxException, IOException {
+		when(cityStateLookup.findCityStateBasedOnZipCode("856585578")).thenReturn(new CityState("Marana", "AZ"));
+		assertCityState("856585578", "Marana", "AZ");
+	}
 
+	private void assertCityState(String zipCode, String city, String state) {
+		CityState cityState = new CityState();
+		try {
+			cityState = cityStateLookup.findCityStateBasedOnZipCode(zipCode);
+			assertEquals(city, cityState.getCity());
+			assertEquals(state, cityState.getState());
+		} catch(AssertionError failure) {
+			fail("Expected: zipCode: " + zipCode + ", city: " + city + ", state: " + state +
+					" Actual: zipCode: " + zipCode + ", city: " + cityState.getCity() + ", state: " + cityState.getState());
+		} catch (Exception e) {
+			fail("Exception: " + e.getMessage());
+		}
+	}
 }
